93d66075fdf158b449e1311adaa2287065bbd490
Angelica <angelica.giraldo10699@gmail.com>
2024-07-13 13:30:25 -0500

    ejercicio completo

--- print.py
+++ print.py
@@ -30,17 +30,53 @@
 
 # nombredeestuduante = "angelica"
 
-nombre = "Tony Soprano"
-edad =  51
-print (nombre )
-print (edad)
+# nombre = "Tony Soprano"
+# edad =  51
+# print (nombre )
+# print (edad)
 
 
-nombre1 = "Julia"
-apellido = "Roberts"
-nombrecompleto = nombre1 + " "+ apellido
-print(nombrecompleto)
+# nombre1 = "Julia"
+# apellido = "Roberts"
+# nombrecompleto = nombre1 + " "+ apellido
+# print(nombrecompleto)
 
-curso = "python"
-print("Estas tomando un curso de: "+ curso)
+# curso = "python"
+# print("Estas tomando un curso de: "+ curso)
 
+#type identifica las varibles 
+
+# num_entero = 10
+# print(type(num_entero))
+
+# num_decimal = 1.564
+# print(type(num_decimal))
+
+# num1 = 7.5
+# num2 = 2.5
+# suma = num1 + num2
+# print (type(suma))
+
+# nombre = "Tony Soprano"
+# edad =  str(51)
+# print (nombre + " "+  edad)
+
+# #formateo de cadenas
+# x=10
+# y=5
+# print("nuumeros {} y {}".format(x,y))
+
+# nombre = "Tony Soprano"
+# edad =  (51)
+# print (f"nombre: {nombre} y edad: {edad}")
+
+#trabajo en clases 2
+
+nombre = input("ingresa un nombre: ")
+edad =  input("ingresa tu edad: ")
+print(f"Bienvenido al sistema {nombre} cuya edad es: {edad}")
+
+numero =  int(input("ingresa un numero: "))
+doble = numero * 2
+print(numero)
+print(doble)
