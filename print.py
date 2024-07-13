769c766efd72858102d942cf2a54d26120164ba7
Angelica <angelica.giraldo10699@gmail.com>
2024-07-13 10:20:49 -0500

    ejercicios en clase

---
+++ print.py
@@ -0,0 +1,30 @@
+#Prints
+# print("Me encanta estudiar python")
+# print("Estudiar con \"python\" es divertido")
+# # print(555/2)
+# # print(555%2)
+# # print(277.5 + 277.5)
+# print(560 - 5)
+
+#Caracteres Especiales
+# print("Linea 1\nLinea 2\nLinea 3\n")
+# print("A\tB\tC\nD\tE\tF\nG\tH\tI")
+
+#imput
+# x= input ("Dime tu nombre: ")
+# print("tu nombre es: " + x)
+
+# print("Tu nombre es: " + (input("Ingresa tu nombre: " )+" "+input("Apellidos: ")))
+
+# # Variable
+# x=input("¿Que estas estudiando?: ")
+# y=input("¿En que pais vives?: ")
+# print("Estudios: " + x +"\nPais: " + y )
+
+
+# print("Estudios: "+ input("¿Que estas estudiando?: "))
+# print("Pais: "+ input("¿En que pais vives?: "))
+
+
+print("Nombre de Cerveza:\"" + (input("¿Cuál es tu color favorito en inglés?: ") + input("Cuales son las primeras 3 letras de tu apellido: "))+"\"")
+
